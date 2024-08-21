5415f721496ee8a04b394402235d8dd9e6c97480
Menotdan <32620310+Menotdan@users.noreply.github.com>
2024-08-21 04:59:54 -0400

    frontend: basic display for reviews

--- frontend/src/components/Review.jsx
+++ frontend/src/components/Review.jsx
@@ -1,7 +1,17 @@
-function Review({ reviewer_name, review_rating, review_text, review_likes }) {
+import { useState } from 'react'
+
+export default function Review({ reviewList, setReviewList, review_id, reviewer_name, review_rating, review_text, review_likes }) {
+    function like() {
+        reviewList[review_id].likes += 1;
+        setReviewList(reviewList);
+    }
+
     return (
         <div id="review">
-            <h3>{ reviewer_name }</h3>
+            <h2 id="reviewer_name">{ reviewer_name }</h2>
+            Rating: <b id="review_rating">{ review_rating }/10</b>
+            <p id="review_text">{ review_text }</p>
+            <button id="like_button" onClick={like}><i id="like_test">{ review_likes }</i> <img id="like_icon" src="assets/like_icon.svg" width={24}></img></button>
         </div>
-    );
+    )
 }
---
+++ frontend/src/components/ReviewList.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from 'react';
+import Review from '/components/Review.jsx'
+
+export default function ReviewList() {
+    const [reviewList, setReviewList] = useState({});
+    const [fetchError, setFetchError] = useState("");
+
+    useEffect(() => {
+        const dataFetch = async () => {
+            try {
+                const response = await fetch(
+                    'http://localhost:8000/api/reviews',
+                );
+
+                if (!response.ok) {
+                    throw new Error(response.status);
+                }
+
+                const data = await response.json();
+
+                let dictData = {};
+                data.forEach(review => {
+                    dictData[review['id']] = review;
+                });
+
+                //console.log(dictData);
+                setReviewList(dictData);
+            }
+            catch (err) {
+                setFetchError(err);
+            }
+        };
+
+        dataFetch();
+    }, []);
+
+    if (fetchError.length > 0) {
+        return (
+            <>
+                <h1>Error!</h1>
+                <p>{fetchError}</p>
+            </>
+        )
+    }
+
+    if (Object.getOwnPropertyNames(reviewList).length == 0) {
+        return (
+            <>
+             <h1>Loading...</h1>
+            </>
+        )
+    }
+
+    return (
+        <div>
+            {console.log(reviewList)}
+            {
+                Object.values(reviewList).map((review, i) =>
+                    <Review
+                        reviewList={reviewList} 
+                        setReviewList={setReviewList}
+                        review_id = {review.id}
+                        reviewer_name = {review.name}
+                        review_rating = {review.rating}
+                        review_text = {review.text}
+                        review_likes = {review.likes}
+                    />
+                )
+            }
+        </div>
+    )
+}
