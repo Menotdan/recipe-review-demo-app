0c76d2f2214a507e55b36f28d29381187907c9a7
Menotdan <32620310+Menotdan@users.noreply.github.com>
2024-08-21 17:25:17 -0400

    backend: return id from create review endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,8 +12,19 @@ class ListReviewsView(generics.ListAPIView):
     serializer_class = ReviewReadSerializer
 
 class CreateReviewView(generics.CreateAPIView):
-    queryset = Review.objects.all()
     serializer_class = ReviewCreationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        id = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(id, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        data = serializer.save()
+        return data.id
 
 class LikeReviewAPIView(APIView):
     def put(self, request, like_id, *args, **kwargs):
