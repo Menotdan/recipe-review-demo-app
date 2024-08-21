07ad10bb7787437becd23f55c2883b0302f9ee59
Menotdan <32620310+Menotdan@users.noreply.github.com>
2024-08-21 00:42:33 -0400

    frontend: Add the start of the frontend

---
+++ frontend/src/components/Review.jsx
@@ -0,0 +1,7 @@
+function Review({ reviewer_name, review_rating, review_text, review_likes }) {
+    return (
+        <div id="review">
+            <h3>{ reviewer_name }</h3>
+        </div>
+    );
+}
